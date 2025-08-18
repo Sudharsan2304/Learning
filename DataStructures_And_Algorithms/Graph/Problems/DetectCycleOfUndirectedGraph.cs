35415b57b2aa526b19c79ceb2cfe6373b5f42765
Sudharsan2304 <sudharsanbalasubramanian1@gmail.com>
2025-08-18 22:52:30 +0530

    Detect cycle of undirected graph

---
+++ DataStructures_And_Algorithms/Graph/Problems/DetectCycleOfUndirectedGraph.cs
@@ -0,0 +1,51 @@
+﻿namespace DataStructures_And_Algorithms.Graph.Problems
+{
+    public partial class GraphProblems
+    {
+
+        public static bool DetectCycle<T>(UndirectedGraph<T> graph) where T : notnull
+        {
+            var visited = new HashSet<T>();
+
+            foreach (var vertex in graph.Vertices)
+            {
+                if (!visited.Contains(vertex))
+                {
+                    if (DetectCycle(graph, vertex, visited, default, hasParent: false))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool DetectCycle<T>(
+            UndirectedGraph<T> graph,
+            T vertex,
+            HashSet<T> visited,
+            T parent,
+            bool hasParent) where T : notnull
+        {
+            visited.Add(vertex);
+
+            foreach (var neighbor in graph.GetNeighbors(vertex))
+            {
+                if (!visited.Contains(neighbor))
+                {
+                    if (DetectCycle(graph, neighbor, visited, vertex, hasParent: true))
+                    {
+                        return true;
+                    }
+                }
+                else if (!(hasParent && EqualityComparer<T>.Default.Equals(neighbor, parent)))
+                {
+                    // visited neighbor that is not the parent → cycle found
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
